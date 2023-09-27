238c45141d2fb8cabf69800b5433cc231e787375
Eduardo Parra <eduparra90@gmail.com>
2023-09-27 23:04:18 +0200

    test

--- .github/scripts/download.cjs
+++ .github/scripts/download.cjs
@@ -1,10 +1,12 @@
 module.exports = async function download({ github, context, core, exec }) {
   core.info("Loading issue data");
-  const { data: { body } } = await octokit.issues.get({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: context.issue.number
-  })
 
-  core.info(body)
+  const { title, body } = github.event.issue;
+
+  core.info(`Issue loaded: ${title}`);
+
+  core.setOutput("title", title);
+  core.setOutput("body", body);
+
+  core.info('Current issue loaded');
 };
