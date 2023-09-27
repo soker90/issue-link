94837e3cd9a8cfdbcc4ecdf91edc75d15c98d0fa
Eduardo Parra <eduparra90@gmail.com>
2023-09-27 22:56:27 +0200

    github copilot

--- .github/scripts/download.cjs
+++ .github/scripts/download.cjs
@@ -1,10 +1,10 @@
 module.exports = async function download({ github, context, core, exec }) {
   core.info("Loading issue data");
-  // const { data: { body } } = await octokit.issues.get({
-  //   owner: context.repo.owner,
-  //   repo: context.repo.repo,
-  //   issue_number: context.issue.number
-  // })
+  const { data: { body } } = await octokit.issues.get({
+    owner: context.repo.owner,
+    repo: context.repo.repo,
+    issue_number: context.issue.number
+  })
 
-  // core.info(body)
+  core.info(body)
 };
