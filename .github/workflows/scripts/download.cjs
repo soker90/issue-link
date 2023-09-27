3fce83dba8945194709df02738169ec2ca40e9e8
Eduardo Parra <eduparra90@gmail.com>
2023-09-27 22:47:58 +0200

    test

--- .github/workflows/scripts/download.cjs
+++ .github/workflows/scripts/download.cjs
@@ -1,11 +1,11 @@
 module.exports = async function download () {
   core.info('Loading issue data')
-  const { data: { body } } = await octokit.issues.get({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    issue_number: context.issue.number
-  })
+  // const { data: { body } } = await octokit.issues.get({
+  //   owner: context.repo.owner,
+  //   repo: context.repo.repo,
+  //   issue_number: context.issue.number
+  // })
 
-  core.info(body)
+  // core.info(body)
 
 }
