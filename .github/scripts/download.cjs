5daa218cf544f2dbfd202381c3fa8a516bf2b3c6
Eduardo Parra <eduparra90@gmail.com>
2023-09-27 23:09:58 +0200

    con copilot

--- .github/scripts/download.cjs
+++ .github/scripts/download.cjs
@@ -1,13 +1,26 @@
+/**
+ * @param {{
+ *  github: InstanceType<typeof import('@actions/github/lib/utils').GitHub>,
+ *  context: import('@actions/github/lib/context').Context,
+ *  core: import('@actions/core'),
+ *  exec: import('@actions/exec')
+ * }}
+ */
 module.exports = async function download({ github, context, core, exec }) {
   core.info("Loading issue data");
 
-  core.info(JSON.stringify(github, null, 2));
-  const { title, body } = github.event.issue;
+  
+  const issue = await github.issues.get({
+    ...context.repo,
+    issue_number: context.issue.number,
+  });
+
+  core.info(JSON.stringify(issue, null, 2));
 
   core.info(`Issue loaded: ${title}`);
 
   core.setOutput("title", title);
   core.setOutput("body", body);
 
-  core.info('Current issue loaded');
+  core.info("Current issue loaded");
 };
