4f7a16e4bb327431d099d045ee896fdb0e4df83b
Eduardo Parra <eduparra90@gmail.com>
2023-09-28 00:08:29 +0200

    imprvements

--- .github/scripts/save-markdown.cjs
+++ .github/scripts/save-markdown.cjs
@@ -1,12 +1,20 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
+
+const toKebabCase = (phrase) => {
+  const words = phrase.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].toLowerCase();
+    words[i] = words[i].replace(/([a-z])([A-Z])/g, "$1-$2");
+  }
+  return words.join("-");
+};
 
 const saveReadmeContent = (string, path, nameFile) =>
-  fs.writeFileSync(`${path}/${nameFile}.md`, string, {
-    encoding: 'utf8',
+  fs.writeFileSync(`${path}/${toKebabCase(nameFile)}.md`, string, {
+    encoding: "utf8",
     overwrite: true,
   });
-
 
 /**
  * @param {{
@@ -16,16 +24,17 @@ const saveReadmeContent = (string, path, nameFile) =>
  *  exec: import('@actions/exec')
  * }}
  */
-module.exports = async function download({ github, context, core, issue }, path) {
+module.exports = async function download(
+  { github, context, core, issue },
+  path
+) {
   core.info("Loading issue data");
 
   const { title, body } = issue;
   core.info(`Issue loaded: ${title}`);
 
-
-  core.info('Updating markdown file');
-  saveReadmeContent(body, path, 'test');
-
+  core.info("Updating markdown file");
+  saveReadmeContent(body, path, title);
 
   core.info("Current markdown file updated");
 };
